67672c6c99a86e8e08ebb7c339364753532d1220
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-08-07 13:30:52 +0530

    add boilerplate for Arrays

--- Arrays/matrixSearch/matrixSearch.java
+++ Arrays/matrixSearch/matrixSearch.java
@@ -3,29 +3,33 @@
 
 
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        
+    public static boolean searchMatrix(int[][] matrix, int target) {
+
         int n_row = matrix.length;
         int n_col = matrix[0].length;
-        
+
         int i = 0, j = n_col - 1;
-        
-        while(i < n_row && j >= 0){
-            
-            if(matrix[i][j] == target)
-            {
+
+        while (i < n_row && j >= 0) {
+
+            if (matrix[i][j] == target) {
                 return true;
             }
-            
-            if(matrix[i][j] < target)
+            if (matrix[i][j] < target)
                 i++;
             else
                 j--;
-                
-            
         }
-        
         return false;
-        
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 24, 60},
+        };
+        int target = 3;
+        System.out.println(searchMatrix(matrix, target));
     }
 }
